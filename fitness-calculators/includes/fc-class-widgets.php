db642d2b7ac7bb141f8dbaffffb729bcd72b5174
gurcharan <mailtogagan990@gmail.com>
2019-06-15 12:41:38 +0530

    Added widget for Body Fat  calculator

--- fitness-calculators/includes/fc-class-widgets.php
+++ fitness-calculators/includes/fc-class-widgets.php
@@ -4,6 +4,7 @@ function Fcpwater_load_widget() {
         register_widget( 'Fcpwater_widget' );
         register_widget( 'Fcpprotein_widget' );
         register_widget( 'Fcpbmi_widget' );
+        register_widget( 'Fcpbfc_widget' );        
         
 }
 add_action( 'widgets_init', 'Fcpwater_load_widget' );
@@ -115,6 +116,48 @@ class Fcpbmi_widget extends WP_Widget {
             }
             else {
             $title = __( 'BMI calculator', 'wpb_widget_domain' );
+            }
+            ?>
+            <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+    <?php 
+    }
+     
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+}
+
+
+// Body Fat calculator
+class Fcpbfc_widget extends WP_Widget {
+ 
+    function __construct() {
+        parent::__construct(        
+        'Fcpbfc_widget',         
+        __('Body Fat calculator', 'Fcpbfc_widget_domain'),         
+        array( 'description' => __( 'Body Fat calculator', 'Fcpbfc_widget_domain' ), ) 
+        );
+    }
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+        echo $args['before_title'] . $title . $args['after_title'];        
+        echo do_shortcode('[fcp-bfc-calculator]');
+        echo $args['after_widget'];
+    }
+         
+    public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+            }
+            else {
+            $title = __( 'Body Fat calculator', 'wpb_widget_domain' );
             }
             ?>
             <p>
