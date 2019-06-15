ebe2339c05f3f9501b468d7e3c4b7a52c90a3f79
gurcharan <mailtogagan990@gmail.com>
2019-06-15 12:12:50 +0530

    Added widget for water intake calculator

--- fitness-calculators/fitness-calculators-plugin.php
+++ fitness-calculators/fitness-calculators-plugin.php
@@ -15,5 +15,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 require_once(dirname(__FILE__) . "/includes/js_css_register.php");
 require_once(dirname(__FILE__) . "/includes/fc-class-short-code.php");
 require_once(dirname(__FILE__) . "/includes/fc-class-settings.php");
+require_once(dirname(__FILE__) . "/includes/fc-class-widgets.php");
 new FcpSettings();
 
--- fitness-calculators/includes/fc-class-widgets.php
+++ fitness-calculators/includes/fc-class-widgets.php
@@ -1,64 +1,54 @@
 <?php
 // Register and load the widget
-function wpb_load_widget() {
-    register_widget( 'wpb_widget' );
+function Fcpwater_load_widget() {
+        register_widget( 'Fcpwater_widget' );
 }
-add_action( 'widgets_init', 'wpb_load_widget' );
+add_action( 'widgets_init', 'Fcpwater_load_widget' );
  
 // Creating the widget 
-class wpb_widget extends WP_Widget {
+class Fcpwater_widget extends WP_Widget {
  
-function __construct() {
-parent::__construct(
+    function __construct() {
+        parent::__construct(        
+        'Fcpwater_widget',         
+        __('Water intake calculator', 'Fcpwater_widget_domain'),         
+        array( 'description' => __( 'Water intake calculator', 'Fcpwater_widget_domain' ), ) 
+        );
+    }
  
-// Base ID of your widget
-'wpb_widget', 
- 
-// Widget name will appear in UI
-__('WPBeginner Widget', 'wpb_widget_domain'), 
- 
-// Widget description
-array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'wpb_widget_domain' ), ) 
-);
-}
- 
-// Creating widget front-end
- 
-public function widget( $args, $instance ) {
-$title = apply_filters( 'widget_title', $instance['title'] );
- 
-// before and after widget arguments are defined by themes
-echo $args['before_widget'];
-if ( ! empty( $title ) )
-echo $args['before_title'] . $title . $args['after_title'];
- 
-// This is where you run the code and display the output
-echo __( 'Hello, World!', 'wpb_widget_domain' );
-echo $args['after_widget'];
-}
+    // Creating widget front-end
+    
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+        echo $args['before_title'] . $title . $args['after_title'];        
+        echo do_shortcode('[fcp-water-intake-calculator]');
+        echo $args['after_widget'];
+    }
          
-// Widget Backend 
-public function form( $instance ) {
-if ( isset( $instance[ 'title' ] ) ) {
-$title = $instance[ 'title' ];
-}
-else {
-$title = __( 'New title', 'wpb_widget_domain' );
-}
-// Widget admin form
-?>
-<p>
-<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-</p>
-<?php 
-}
+    // Widget Backend 
+    public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+            }
+            else {
+            $title = __( 'Water intake calculator', 'wpb_widget_domain' );
+            }
+            ?>
+            <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+    <?php 
+    }
      
-// Updating widget replacing old instances with new
-public function update( $new_instance, $old_instance ) {
-$instance = array();
-$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-return $instance;
-}
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
 } 
+
+
 ?>
