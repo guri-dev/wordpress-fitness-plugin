eaa97dab2411308916acebd192db86c5c338f076
gurcharan <mailtogagan990@gmail.com>
2019-06-15 12:15:59 +0530

    Added widget for protein  intake calculator

--- fitness-calculators/includes/fc-class-widgets.php
+++ fitness-calculators/includes/fc-class-widgets.php
@@ -5,7 +5,7 @@ function Fcpwater_load_widget() {
 }
 add_action( 'widgets_init', 'Fcpwater_load_widget' );
  
-// Creating the widget 
+// water intake calculaltor
 class Fcpwater_widget extends WP_Widget {
  
     function __construct() {
@@ -15,9 +15,6 @@ class Fcpwater_widget extends WP_Widget {
         array( 'description' => __( 'Water intake calculator', 'Fcpwater_widget_domain' ), ) 
         );
     }
- 
-    // Creating widget front-end
-    
     public function widget( $args, $instance ) {
         $title = apply_filters( 'widget_title', $instance['title'] );
         echo $args['before_widget'];
@@ -27,7 +24,6 @@ class Fcpwater_widget extends WP_Widget {
         echo $args['after_widget'];
     }
          
-    // Widget Backend 
     public function form( $instance ) {
     if ( isset( $instance[ 'title' ] ) ) {
             $title = $instance[ 'title' ];
@@ -48,7 +44,48 @@ class Fcpwater_widget extends WP_Widget {
         $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
         return $instance;
     }
-} 
+}
+
+// protein intake calculator
+class Fcpprotein_widget extends WP_Widget {
+ 
+    function __construct() {
+        parent::__construct(        
+        'Fcpprotein_widget',         
+        __('Protein intake calculator', 'Fcpprotein_widget_domain'),         
+        array( 'description' => __( 'Protein intake calculator', 'Fcpprotein_widget_domain' ), ) 
+        );
+    }
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        echo $args['before_widget'];
+        if ( ! empty( $title ) )
+        echo $args['before_title'] . $title . $args['after_title'];        
+        echo do_shortcode('[fcp-protein-intake-calculator]');
+        echo $args['after_widget'];
+    }
+         
+    public function form( $instance ) {
+    if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+            }
+            else {
+            $title = __( 'Protein intake calculator', 'wpb_widget_domain' );
+            }
+            ?>
+            <p>
+            <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+            <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+            </p>
+    <?php 
+    }
+     
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+}
 
 
 ?>
